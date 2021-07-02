da0ae6c966902249bd4c12587359f7c17da35a19
Joel Ekberg <leoj.bergek@gmail.com>
2021-07-02 17:47:20 +0200

    AISTUFF

---
+++ Gravity-Thunder-Stealth-Punch/Assets/Joel/Scripts/EnemyGuard_.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGuard_ : EnemyBase_
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    override protected void Start()
+    {
+        base.Start();
+    }
+
+    override protected void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+}
