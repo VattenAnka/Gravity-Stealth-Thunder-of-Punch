84a0c52a0ac1ab84d4231d25dfb29d198f9388c2
Theo <theo.liem.norell@gmail.com>
2021-09-23 23:47:38 +0200

    Added force push and force push jump

--- Gravity-Thunder-Stealth-Punch/Assets/Theo/PlayerCharacter/Scripts/InputManager.cs
+++ Gravity-Thunder-Stealth-Punch/Assets/Theo/PlayerCharacter/Scripts/InputManager.cs
@@ -7,23 +7,30 @@ public class InputManager : MonoBehaviour
 {
     PlayerLocomotion playerLocomotion;
     AnimatorManager animatorManager;
-    [HideInInspector] public float horizontalInput, verticalInput ;
+    PlayerAbilities playerAbilities;
+    [HideInInspector] public float horizontalInput, verticalInput;
     [HideInInspector] public bool jumpInput;
-    
+
     public float moveAmount;
-     public float cameraInputX, cameraInputY;
+    public float cameraInputX, cameraInputY;
     public Vector3 mousePos;
     bool isMoving;
     private void Awake()
     {
+        playerAbilities = GetComponent<PlayerAbilities>();
         animatorManager = GetComponent<AnimatorManager>();
         playerLocomotion = GetComponent<PlayerLocomotion>();
     }
     public void HandleAllInputs()
     {
         //hide cursor when clicking on screen
-        if (Input.GetMouseButtonDown(0)) Cursor.visible = false;
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            animatorManager.PlayTargetAnimation("Punch", false);
+            playerAbilities.ForcePush();
+            Cursor.visible = false;
+        }
+
         HandleMovementInput();
         HandleJumpInput();
         //Handle Camera
@@ -43,7 +50,7 @@ public class InputManager : MonoBehaviour
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
         isMoving = !(horizontalInput == 0 && verticalInput == 0);
-      
+
         cameraInputX = Input.GetAxis("Mouse X");
         cameraInputY = Input.GetAxis("Mouse Y");
         animatorManager.UpdateAnimatorValues(moveAmount);
@@ -55,11 +62,11 @@ public class InputManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftControl)) playerLocomotion.isRunning = !playerLocomotion.isRunning;
         playerLocomotion.isSprinting = Input.GetKey(KeyCode.LeftShift);
 
-        if (!playerLocomotion.isSprinting&&playerLocomotion.isRunning && isMoving) moveAmount = .6f;
+        if (!playerLocomotion.isSprinting && playerLocomotion.isRunning && isMoving) moveAmount = .6f;
         else if (playerLocomotion.isSprinting && isMoving) moveAmount = 1f;
         else if (!playerLocomotion.isRunning && isMoving) moveAmount = 0.35f;
         else moveAmount = 0;
     }
 
-    
+
 }
---
+++ Gravity-Thunder-Stealth-Punch/Assets/Theo/PlayerCharacter/Scripts/PlayerAbilities.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAbilities : MonoBehaviour
+{
+    Camera camera;
+    RaycastHit hit;
+    public float maxForce;
+    float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawLine(camera.transform.position, hit.point);
+
+    }
+
+    public void ForcePush()
+    {
+
+        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit))
+        {
+
+            Debug.Log(hit.collider.gameObject);
+
+            if (hit.collider.gameObject.GetComponent<Rigidbody>() != null)
+            {
+                force = 0;
+                if (maxForce - Vector3.Distance(hit.point, camera.transform.position)>0) force = maxForce- Vector3.Distance(hit.point, camera.transform.position);
+                hit.collider.gameObject.GetComponent<Rigidbody>().AddForce((hit.point - camera.transform.position) * force, ForceMode.Impulse);
+            }
+            else
+            {
+                force = 0;
+                if (maxForce - Vector3.Distance(hit.point, camera.transform.position) > 0) force = maxForce - Vector3.Distance(hit.point, camera.transform.position);
+                GetComponent<Rigidbody>().AddForce(-(hit.point - camera.transform.position) * force/4, ForceMode.Impulse);
+            }
+
+                
+
+
+        }
+    }
+}
+
